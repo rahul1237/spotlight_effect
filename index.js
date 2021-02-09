a0104fd8d6dcbbe31465995af930f801e544a2a6
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-10 03:38:45 +0530

    spotlight_effect added

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const main = document.querySelector('.main_box')
 
 main.addEventListener('mousemove',(e)=>{
     const mouse = document.querySelector('.mouse')
-    mouse.style.clipPath = `circle(4em at ${e.offsetX}px ${e.offsetY}px)`
+    mouse.style.clipPath = `circle(10em at ${e.offsetX}px ${e.offsetY}px)`
 })
